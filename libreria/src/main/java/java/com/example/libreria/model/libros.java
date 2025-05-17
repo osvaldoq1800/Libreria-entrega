b150a9d4222d50a6ba4691e08fdafaecfd6f0a5d
PAAPCA03LC1209 <PAAPCA03LC1209@PAAPCA03LC1209.duoc.acad>
2025-05-16 20:40:06 -0400

    mi aporte

--- libreria/src/main/java/java/com/example/libreria/model/libros.java
+++ libreria/src/main/java/java/com/example/libreria/model/libros.java
@@ -1,5 +1,7 @@
 package com.example.libreria.model;
 
+import javax.print.DocFlavor.STRING;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,4 +18,5 @@ public class libros {
     private int fechapublicacion;
     private String autor;
 
+    private String elpixula;
 }
